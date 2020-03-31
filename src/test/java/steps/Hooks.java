9152370c4be6be513fee7c605f3988ddcf02408a
SebsCubs <ascespedesc@unal.edu.co>
2020-03-31 16:25:58 -0500

    Adding ChromeOptions for linux execution

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -8,18 +8,30 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Hooks {
-    private static WebDriver webDriver;
+    private WebDriver webDriver;
 
     @Before
     public void setup() {
         // Set up driver factory
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("enable-automation");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-infobars");
+        options.addArguments("--disable-dev-sha-usage");
+        options.addArguments("--disable-browser-side-navigation");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--headless");
+
         String chromeDriverPath = "/usr/bin/chromedriver";
         System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-        webDriver = new ChromeDriver();
+        webDriver = new ChromeDriver(options);
+
         webDriver.manage().window().maximize();
-        webDriver.get("https://www.falabella.com.co/falabella-co/");
+        webDriver.navigate().to("https://www.falabella.com.co/falabella-co/");
     }
 
     @After
