19735e109b45ede2cf5a3501bd64ee35acbc5393
SebsCubs <ascespedesc@unal.edu.co>
2020-04-01 08:33:38 -0500

    Fixed screen size problem, added all features in runner

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features/shopping.feature",
+@CucumberOptions(features = "src/test/resources/features/",
         glue = "steps",
         plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
 public class TestRunner {
